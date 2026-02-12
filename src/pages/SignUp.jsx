ec3db935a9e9ecb3ead3a4d60250b1d269f621d9
madhav <madhavjoshi6280@gmail.com>
2026-02-12 23:22:19 +0530

    sign-up name check done

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -219,6 +219,7 @@ export function SignUp() {
   const [password, setPassword] = useState('')
   const [confirmPass, setConfirmPass] = useState('')
 
+  const [nameCheck, setNameCheck] = useState(false)
   const [emailErr, setEmailErr] = useState(false)
   const [passErr, setPassErr] = useState(false)
   const [passCheck, setPassCheck] = useState(false)
@@ -277,13 +278,21 @@ export function SignUp() {
                 placeholder="Enter your name"
                 value={name}
                 onChange={(e) => {
-                  setName(e.target.value)
+                  // setNameCheck(true)
+                  const value = e.target.value
+                  if(/[0-9`~!@#$%^&*()_+=?/><.,-]/.test(value)){
+                    setNameCheck(false)
+                  }else{
+                    setNameCheck(true)
+                    setName(value)
+                  }
                 }}
                 className="w-full text-black! placeholder-black! rounded-lg border border-gray-200 px-4 py-2.5 text-sm focus:ring-2 focus:ring-indigo-400"
               />
+              {/* {/[0-9`~!@#$%^&*()_+=?/><.,-]/.test(name) ? setNameCheck(false) : ''} */}
               {/* {/[0-9]/.test(name)? <span className='text-red-500 text-[12px]'>Name can't include any number or symbol</span> :''}
             </div> */}
-              {/[0-9`~!@#$%^&*()_+=?/><.,-]/.test(name) ? setName('') : ''}
+              {/* {/[0-9`~!@#$%^&*()_+=?/><.,-]/.test(name) ? setName('') : ''} */}
             </div>
 
             <div className="text-left">
